2cf49b7db4b1877163810e1ecca37211e9772ae2
Rich Jones <jones_rich@yahoo.com>
2020-10-21 17:39:08 -0500

    first card view; rich test branch

--- resources/views/livewire/riches-section.blade.php
+++ resources/views/livewire/riches-section.blade.php
@@ -1,11 +1,22 @@
-<div class="mt-10 text-5xl">
-    This is rich's section
+{{--<div class="mt-10 text-5xl">--}}
+<div>
+{{--    This is rich's section--}}
 
-    <div class="flex items-center justify-start mt-2">
-        <span>{{ $count }}</span>
-        <button wire:click="decrement" class="ml-5 mr-1">[-]</button>
-        <button wire:click="increment">[+]</button>
-    </div>
+{{--    <div class="flex items-center justify-start mt-2">--}}
+{{--        <span>{{ $count }}</span>--}}
+{{--        <button wire:click="decrement" class="ml-5 mr-1">[-]</button>--}}
+{{--        <button wire:click="increment">[+]</button>--}}
+{{--    </div>--}}
+
+{{--    <img class="img-responsive img-rounded" src="{{ asset('img/aceOfSpades.jpg')  }}" alt="Italian Trulli">--}}
+
+    <img alt="ace of spades card is missing..." src="{{ asset('img/aceOfSpades.jpg') }}"
+         srcset="{{asset('img/aceOfSpades.jpg 860w')}}
+             ,{{asset('img/aceOfSpades.jpg 640w')}}
+             ,{{asset('img/aceOfSpades.jpg 420w')}}
+             ,{{asset('img/aceOfSpades.jpg 320w')}}">
+
+
 </div>
 
 
--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -15,6 +15,10 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+
+                    <x-jet-nav-link href="{{ route('decks') }}" :active="request()->routeIs('decks')">
+                        {{ __('Decks') }}
+                    </x-jet-nav-link>
                 </div>
             </div>
 
