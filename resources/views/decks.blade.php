ac02d64628a00b9fba038752b8ff55027e7fd02d
Rich Jones <jones_rich@yahoo.com>
2020-10-27 14:20:40 -0500

    misc work on the blade side; added person table and example code

--- resources/views/decks.blade.php
+++ resources/views/decks.blade.php
@@ -1,16 +1,21 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Decks') }}
+{{--            {{ __('Decks') }}--}}
+            <nav class="rounded bg-gray-900 text-white px-4 py-3 flex items-centered justify-between">
+                <div class="flex items-center space-x-4">
+                    <div>logo</div>
+                    <div>search box</div>
+                    <div>other options</div>
+                </div>
+                <div>right</div>
+            </nav>
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12 m-8">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <livewire:riches-section />
-
-{{--            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">--}}
-{{--            </div>--}}
         </div>
     </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Person;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/person', function (Request $request) {
+
+    $hairColor = $request->input("color");
+
+    $person = new Person;
+
+    $person->setAttribute("hair_color",$hairColor);
+
+    if ($person->save()) {
+        echo "you saved a person with a hair colored ". $hairColor;
+    };
+});
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
