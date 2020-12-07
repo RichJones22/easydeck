584bec5e6ff8e0332e13766475c2ff465b4f6327
Rich Jones <jones_rich@yahoo.com>
2020-12-06 19:09:20 -0600

    added a 'dz-card-file-upload' event to the queuecomplete function in dz-card-file-upload.blade.php file

--- app/Http/Livewire/DzCardFileUpload.php
+++ app/Http/Livewire/DzCardFileUpload.php
@@ -6,6 +6,8 @@ use Livewire\Component;
 
 class DzCardFileUpload extends Component
 {
+    protected $listeners = ['dz-card-file-upload' => 'render'];
+
     public function render()
     {
         return view('livewire.dz-card-file-upload');
