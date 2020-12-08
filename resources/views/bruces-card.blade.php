aa1c725259a00e806d0d93dbb86a692ea9c92ba7
Bruce Howard <burousu@Bruces-MacBook-Pro.local>
2020-12-07 19:42:05 -0500

    git restore delelted files

--- resources/views/bruces-card.blade.php
+++ resources/views/bruces-card.blade.php
@@ -1,19 +1,18 @@
 <x-guest-layout>
-        <div class="flex flex-col justify-center items-center">
-{{--    <div class="py-12">--}}
-{{--        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">--}}
-            <div>
-             <div class="mx-auto sm:px-6 lg:px-8 flex justify-between">
-                    <div>Logo</div>
-                    <div>Right</div>
+        <div>
+    <div class="py-12">
+        <div class="mx-auto sm:px-48 lg:px-64">
+            <div class="flex justify-between bg-gray-200 p-2">
+                <div>Logo</div>
+                <div>Right</div>
             </div>
-            <div class="border border-grey-200" >
-                <livewire:bruces-section />
-            </div>
+                <div class="border border-grey-200">
+                    <livewire:bruces-section />
+                </div>
             <div>
-                    <p>
-                         Bruce Howard &copy 2000
-                    </p>
+            <p class="bg-gray-200 p-2">
+                 Bruce Howard &copy 2020
+            </p>
 
              </div>
         </div>
